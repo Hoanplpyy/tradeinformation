83d4cea0564cced181883d6e5babc46898138bff
hsien <hoanplpyy@hotmail.com>
2022-05-25 16:13:36 +0800

    un flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { NewsComponent } from './news/news.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared.module';
 import { googleChartsConfigFactory, GoogleChartsConfigService } from './googleChart.service';
-import { FlexLayoutModule   } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +35,7 @@ import { FlexLayoutModule   } from '@angular/flex-layout';
     NgbModule,
     BrowserAnimationsModule,
     SharedModule,
-    FlexLayoutModule
+
   ],
    providers: [
     //GoogleChartsConfigService
